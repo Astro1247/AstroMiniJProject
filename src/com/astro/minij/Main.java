df633902603e60c7cb1a78d2d415a70cdb6f8f42
Astro1247 <taynik777@gmail.com>
2021-01-22 16:48:35 +0200

    Init project

---
+++ src/com/astro/minij/Main.java
@@ -0,0 +1,9 @@
+package com.astro.minij;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: Собрать аптечку ПМП
+        System.out.println("Welcome to MiniJ project!");
+    }
+}
