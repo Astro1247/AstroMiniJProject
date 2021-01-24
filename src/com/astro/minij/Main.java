38669f9ddd06b1ccb3047afb6bba2ec10d5f61c0
Astro1247 <taynik777@gmail.com>
2021-01-24 21:26:59 +0200

    Single and multi thread performance comparsion

--- src/com/astro/minij/Main.java
+++ src/com/astro/minij/Main.java
@@ -7,9 +7,14 @@ import com.astro.minij.models.kits.FirstAidKit;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Welcome to MiniJ project!");
-        int n = 100;
+        threadingComparsionOnMatrixVectorMultiply();
+    }
+
+    public static void threadingComparsionOnMatrixVectorMultiply() throws InterruptedException {
+        int n = 1000;
+        int threads = 100;
         Matrix matrix = new Matrix(n, n);
         for (int i = 0; i < matrix.getWidth(); i++) {
             for (int j = 0; j < matrix.getHeight(); j++) {
@@ -22,12 +27,44 @@ public class Main {
         }
         long minSingleThreadExecTime = Long.MAX_VALUE;
         long maxSingleThreadExecTime = Long.MIN_VALUE;
+        long startSingleTime = System.nanoTime();
         for (int i = 0; i < 100; i++) {
             long execTime = singleThreadMatrixVectorMultiplier(matrix, vector);
             if (minSingleThreadExecTime > execTime) minSingleThreadExecTime = execTime;
             if (maxSingleThreadExecTime < execTime) maxSingleThreadExecTime = execTime;
         }
+        long stopSingleTime = System.nanoTime();
         System.out.println("Single thread:\nMin exec time: " + minSingleThreadExecTime + "\nMax exec time: " + maxSingleThreadExecTime);
+        System.out.println("Total measurement time took: " + (stopSingleTime - startSingleTime));
+
+        //ThreadMult threadSample = new ThreadMult(matrix, vector, 5);
+        //threadSample.run();
+
+        //singleThreadMatrixVectorMultiplier(matrix, vector);
+        System.out.println("==========");
+        //multiThreadMatrixVectorMultiplier(matrix, vector, 5);
+
+        long minMultiThreadExecTime = Long.MAX_VALUE;
+        long maxMultiThreadExecTime = Long.MIN_VALUE;
+        long startMultiTime = System.nanoTime();
+        for (int i = 0; i < 100; i++) {
+            long execTime = multiThreadMatrixVectorMultiplier(matrix, vector, threads);
+            if (minMultiThreadExecTime > execTime) minMultiThreadExecTime = execTime;
+            if (maxMultiThreadExecTime < execTime) maxMultiThreadExecTime = execTime;
+        }
+        long stopMultiTime = System.nanoTime();
+        System.out.println("Multi thread:\nMin exec time: " + minSingleThreadExecTime + "\nMax exec time: " + maxSingleThreadExecTime);
+        System.out.println("Total measurement time took: " + (stopMultiTime - startMultiTime));
+
+        System.out.println("==========");
+
+        if((stopMultiTime - startMultiTime) < (stopSingleTime - startSingleTime)) {
+            System.out.println("So, multi thread was faster by: " + ((stopSingleTime - startSingleTime) - (stopMultiTime - startMultiTime)));
+        } else if ((stopMultiTime - startMultiTime) > (stopSingleTime - startSingleTime)) {
+            System.out.println("So, single thread was faster by: " + ((stopMultiTime - startMultiTime) - (stopSingleTime - startSingleTime)));
+        } else {
+            System.out.println("So, single and multi thread comparsion got same exec time. WOW! IMPOSSIBLE!");
+        }
     }
 
     public static long singleThreadMatrixVectorMultiplier(Matrix matrix, Vector vector) {
@@ -40,6 +77,34 @@ public class Main {
             }
             resultVector.setValue(i, newValue);
         }
+        long stopTime = System.nanoTime();
+        //resultVector.display();
+        //System.out.println("Single thread execution time: " + (stopTime - startTime));
+        return stopTime - startTime;
+    }
+
+    public static long multiThreadMatrixVectorMultiplier(Matrix matrix, Vector vector, int threads) throws InterruptedException {
+        long startTime = System.nanoTime();
+        ThreadMult[] threadArray = new ThreadMult[threads];
+        int rows = matrix.getHeight();
+
+        for (int i = 0; i < threads; i++) {
+            threadArray[i] = new ThreadMult(matrix ,vector,
+                    rows/threads * i,
+                    i==threads-1 ?rows:rows/threads * (i + 1) );
+            threadArray[i].start();
+        }
+        for (int i = 0; i < threads; i++){
+            threadArray[i].join();
+        }
+        Vector resultVector = new Vector(rows);
+        int inserted = 0;
+        for (int i = 0; i < threads; i++){
+            for (int j = 0; j < threadArray[i].resultVector.getSize(); j++) {
+                resultVector.setValue(inserted++, threadArray[i].resultVector.getValue(j));
+            }
+        }
+        //resultVector.display();
         long stopTime = System.nanoTime();
         //System.out.println("Single thread execution time: " + (stopTime - startTime));
         return stopTime - startTime;
@@ -56,5 +121,40 @@ public class Main {
         firstAidKit.addMedication(analgin);
         System.out.println(firstAidKit.medications.size());
         System.out.println(firstAidKit.toString());
+    }
+}
+
+class ThreadMult extends Thread {
+    public Matrix matrix;
+    public Vector vector;
+    public Vector resultVector;
+    public int inserted;
+
+    public int startRow;
+    public int endRow;
+
+    public ThreadMult(Matrix matrix, Vector vector, int startRow, int endRow) {
+        this.matrix = matrix;
+        this.vector = vector;
+        this.startRow = startRow;
+        this.endRow = endRow;
+        resultVector = new Vector(endRow-startRow);
+        inserted = 0;
+    }
+
+    public Vector getResult() {
+        return resultVector;
+    }
+
+    @Override
+    public void run(){
+        for (int i = startRow; i < endRow; i++) {
+        //for (int i = 0; i < endRow-startRow; i++) {
+            float newValue = 0;
+            for (int j = 0; j < matrix.getWidth(); j++) {
+                newValue += matrix.getValue(j, i)*vector.getValue(j);
+            }
+            resultVector.setValue(inserted++, newValue);
+        }
     }
 }
